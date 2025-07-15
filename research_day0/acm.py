7468ec856ecf4cffac0500cc12ba10d63304cb6f
Roman Yasinovskyy <yasinovskyy@users.noreply.github.com>
2025-07-15 13:29:50 -0500

    Update docstring

--- research_day0/acm.py
+++ research_day0/acm.py
@@ -1,4 +1,18 @@
-def read_txt(filename, state):
+#!/usr/bin/env python3
+"""
+Data handling demo
+
+@author: Roman Yasinovskyy
+@version: 2025.7
+"""
+
+
+def read_txt(filename: str, state: str) -> None:
+    """Read data from a text file
+
+    :param filename: name of a file to open
+    :param state: state to filter by
+    """
     with open(filename, "r", encoding="utf-8") as input_file:
         data = input_file.readlines()
         for college in data:
